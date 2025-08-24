55c8066cbd3399e3111e68b08945ceb19f095adf
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2025-08-24 23:18:44 +0530

    CRUD Added

--- 1-demo/app/Http/Controllers/EmployeeController.php
+++ 1-demo/app/Http/Controllers/EmployeeController.php
@@ -6,6 +6,26 @@ class EmployeeController extends Controller
 {
     public function create()
     {
-        return response()->json('Employee created');
+        return response()->json('Employee created successfully');
+    }
+
+    public function update()
+    {
+        return response()->json('Employee updated successfully');
+    }
+
+    public function delete()
+    {
+        return response()->json('Employee deleted successfully');
+    }
+
+    public function get()
+    {
+        return response()->json('Employee retrieved successfully');
+    }
+
+    public function getAll()
+    {
+        return response()->json('All employees retrieved successfully');
     }
 }
--- 1-demo/routes/api.php
+++ 1-demo/routes/api.php
@@ -5,4 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/employee')->group(function () {
     Route::post('/create', [EmployeeController::class, 'create']);
+    Route::patch('/update/{id}', [EmployeeController::class, 'update']);
+    Route::delete('/delete/{id}', [EmployeeController::class, 'delete']);
+    Route::get('/get/{id}', [EmployeeController::class, 'get']);
+    Route::get('/get-all', [EmployeeController::class, 'getAll']);
 });
